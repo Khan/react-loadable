b4a84c223f1182739e78fadf5d10a6ccb90b8e20
Kevin Barabash <kevinb@khanacademy.org>
2020-05-10 01:23:10 -0400

    Prefix unsafe lifecycle methods with UNSAFE_
    Summary:
    In order to use ReactLoadable with React 16.9 without React logging
    warning we need to prefix unsafe lifecycle methods with UNSAFE_.
    
    I update react, react-dom, and react-test-utils to 16.9 since the
    current version we were using didn't know about UNSAFE_componentWillMount()
    and as a result most of the tests failed.
    
    Reviewers: #fe-infra

--- src/index.js
+++ src/index.js
@@ -162,7 +162,7 @@ function createLoadableComponent(loadFn, options) {
       return init();
     }
 
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
       this._loadModule();
     }
 
